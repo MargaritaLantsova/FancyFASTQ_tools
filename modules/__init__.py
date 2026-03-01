fa126ee9eb9b877ea354b4de0be13441034611af
Margarita Lantsova <114350855+MargaritaLantsova@users.noreply.github.com>
2026-03-01 23:09:38 +0300

    Delete modules/__init__.py

--- modules/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-HW16 moved all functional code into main.py:
-- OOP sequence classes (DNASequence/RNASequence/AminoAcidSequence)
-- FASTQ filtering via Biopython
-This package is kept temporarily to avoid import errors in older code.
-It will be removed.
-"""
-
-__all__ = []
