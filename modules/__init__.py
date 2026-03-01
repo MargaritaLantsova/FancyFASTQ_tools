013b12c02e881785d50ab95ba800a422e064ac53
Margarita Lantsova <114350855+MargaritaLantsova@users.noreply.github.com>
2026-03-01 22:38:10 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,47 +1,9 @@
 """
-This package contains core modules for bioinformatics sequence analysis:
-- sequence_tools: DNA/RNA sequence manipulation utilities
-- fastq_tools: FASTQ file processing and filtering utilities
+HW16 moved all functional code into main.py:
+- OOP sequence classes (DNASequence/RNASequence/AminoAcidSequence)
+- FASTQ filtering via Biopython
+This package is kept temporarily to avoid import errors in older code.
+It will be removed.
 """
 
-from .sequence_tools import (
-    is_valid_nucleic_acid,
-    is_dna,
-    is_rna,
-    transcribe,
-    reverse,
-    complement,
-    reverse_complement,
-    gc_content,
-    OPERATIONS,
-)
-
-from .fastq_tools import (
-    calculate_avg_quality,
-    check_bounds,
-    filter_sequence,
-    DEFAULT_GC_BOUNDS,
-    DEFAULT_LENGTH_BOUNDS,
-    DEFAULT_QUALITY_THRESHOLD,
-)
-
-# Define what gets imported with "from modules import *"
-__all__ = [
-    # Sequence tools
-    "is_valid_nucleic_acid",
-    "is_dna",
-    "is_rna",
-    "transcribe",
-    "reverse",
-    "complement",
-    "reverse_complement",
-    "gc_content",
-    "OPERATIONS",
-    # FASTQ tools
-    "calculate_avg_quality",
-    "check_bounds",
-    "filter_sequence",
-    "DEFAULT_GC_BOUNDS",
-    "DEFAULT_LENGTH_BOUNDS",
-    "DEFAULT_QUALITY_THRESHOLD",
-]
+__all__ = []
